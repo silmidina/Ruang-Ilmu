e2ff3d2d074989f559717fbb40ffe3e486931f5d
silmidina <silmirhmdn08@gmail.com>
2025-02-16 19:57:29 +0700

    membuat username generator

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\Models\User;
+
 if (!function_exists('flashMessage')) {
   function flashMessage($message, $type = 'success'): void
   {
     session()->flash('message', $message);
     session()->flash('type', $type);
+  }
+}
+
+if (!function_exists('usernameGenerator')) {
+  function usernameGenerator(string $name): string
+  {
+    $username = strtolower(preg_replace('/\s+/', '_', trim($name)));
+    $original_username = $username;
+    $count = 1;
+    while (User::where('username', $username)->exists()) {
+      $username = $original_username . $count;
+      $count++;
+    }
+    return $username;
   }
 }
