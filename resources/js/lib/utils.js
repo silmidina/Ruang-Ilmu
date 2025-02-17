7e63db5f2914b856604f6dda48bde228fdf0b17d
silmidina <silmirhmdn08@gmail.com>
2025-02-17 20:56:19 +0700

    message error

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -27,3 +27,37 @@ export const formatToRupiah = (amount)=>{
 
   return formatter.format(amount);
 }
+
+// Message Error
+export const messages = {
+  503: {
+    title: 'Service Unavailable',
+    description: 'Sorry, we are doing some maintenance. Please check back soon',
+    status: '503',
+  },
+  500: {
+    title: 'Server Error',
+    description: 'Oops, something went wrong on our servers',
+    status: '500',
+  },
+  404: {
+    title: 'Not Found',
+    description: 'Sorry, the page you are looking for could not be found',
+    status: '404',
+  },
+  403: {
+    title: 'Forbidden',
+    description: 'Sorry, you are forbidden from accessing this page',
+    status: '403',
+  },
+  401: {
+    title: 'Unauthorized',
+    description: 'Sorry, you are unauthorized to access this page',
+    status: '401',
+  },
+  429: {
+    title: 'Too Many Request',
+    description: 'Please try again in just a second',
+    status: '429',
+  },
+}
