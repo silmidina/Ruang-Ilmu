0d37086fa5bfaaa10b03b9f3e209002b02817183
silmidina <silmirhmdn08@gmail.com>
2025-02-21 09:19:19 +0700

    active menu sidebar

--- resources/js/Layouts/Partials/SidebarResponsive.jsx
+++ resources/js/Layouts/Partials/SidebarResponsive.jsx
@@ -22,13 +22,13 @@ import {
     IconVersions,
 } from '@tabler/icons-react';
 
-export default function SidebarResponsive() {
+export default function SidebarResponsive({url}) {
     return (
         <nav className="grid gap-6 text-lg font-medium">
             <ApplicationLogo />
             <nav className="grid items-start text-sm font-semibold lg:px-4">
                 <div className="px-3 py-2 text-sm font-semibold text-foreground">Dashboard</div>
-                <NavLinkResponsive url="#" title="Dashboard" icon={IconDashboard} />
+                <NavLinkResponsive url={route('dashboard')} active={url.startsWith('/dashboard')} title="Dashboard" icon={IconDashboard} />
 
                 <div className="px-3 py-2 text-sm font-semibold text-foreground">Statistik</div>
                 <NavLinkResponsive url="#" title="Statistik Peminjaman" icon={IconChartDots2} />
