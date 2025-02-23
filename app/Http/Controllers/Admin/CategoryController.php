a51fab6ecf0578f527572cbf4564af5d0c6411b2
silmidina <silmirhmdn08@gmail.com>
2025-02-23 20:34:36 +0700

    tampilkan data kategori

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Inertia\Response;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $categories = Category::query()
+            ->select(['id', 'name', 'slug', 'cover', 'created_at'])
+            ->get();
+        return inertia('Admin/Categories/Index', [
+            'categories' => $categories,
+            'page_settings' => [
+                'title' => 'Kategori',
+                'subtitle' => 'Menampilkan semua data kategori yang tersedia pada platform ini.',
+
+            ]
+        ]);
+    }
 }
