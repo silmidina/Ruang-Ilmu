9cf267d065b57bb8607c7723bebc39f3f87a1c55
silmidina <silmirhmdn08@gmail.com>
2025-02-24 21:03:47 +0700

    scope kategori

--- resources/js/Pages/Admin/Categories/Index.jsx
+++ resources/js/Pages/Admin/Categories/Index.jsx
@@ -11,7 +11,7 @@ import { UseFilter } from "@/hooks/UseFilter"
 import AppLayout from "@/Layouts/AppLayout"
 import { flashMessage } from "@/lib/utils"
 import { Link, router } from "@inertiajs/react"
-import { IconArrowsDownUp, IconCategory, IconPencil, IconPlus, IconTrash } from "@tabler/icons-react"
+import { IconArrowsDownUp, IconCategory, IconPencil, IconPlus, IconRefresh, IconTrash } from "@tabler/icons-react"
 import { useState } from "react"
 import { toast } from "sonner"
 
@@ -76,6 +76,14 @@ export default function Index(props) {
                 ))}
               </SelectContent>
             </Select>
+            <Button
+              variant="red"
+              onClick={() => setParams(props.state)}
+              size="xl"
+            >
+              <IconRefresh className="size-4" />
+              Bersihkan
+            </Button>
           </div>
         </CardHeader>
         <CardContent className="px-0 py-0 [&-td]:whitespace-nowrap [&_td]:px-6 [&_th]:px-6">
