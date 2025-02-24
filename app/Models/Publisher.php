4d5f2003ea7e3920719806e19a10cd16ad9482eb
silmidina <silmirhmdn08@gmail.com>
2025-02-24 21:47:14 +0700

    scope filter dan sorting

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 
 class Publisher extends Model
 {
@@ -22,5 +23,26 @@ class Publisher extends Model
     public function books(): HasMany
     {
         return $this->hasMany(Book::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->whereAny([
+                    'name',
+                    'slug',
+                    'email',
+                    'address',
+                    'phone'
+                ], 'REGEXP', $search);
+            });
+        });
+    }
+    public function scopeSorting(Builder $query, array $sorts): void
+    {
+        $query->when($sorts['field'] ?? null && $sorts['direction'] ?? null, function ($query) use ($sorts) {
+            $query->orderBy($sorts['field'], $sorts['direction']);
+        });
     }
 }
