96f220e26a01773768d48adc0c1ef7a9b76af243
silmidina <silmirhmdn08@gmail.com>
2025-02-26 23:29:11 +0700

    menyiapkan route dan controller admin pengaturan denda

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\FineSettingController;
 use App\Http\Controllers\Admin\PublisherController;
 use App\Http\Controllers\Admin\UserController;
 use App\Models\Publisher;
@@ -42,5 +43,10 @@ Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('users/edit/{user}', 'edit')->name('admin.users.edit');
     Route::put('users/edit/{user}', 'update')->name('admin.users.update');
     Route::delete('users/destroy/{user}', 'destroy')->name('admin.users.destroy');
+  });
+
+  Route::controller(FineSettingController::class)->group(function () {
+    Route::get('fine-settings/create', 'create')->name('admin.fine-settings.create');
+    Route::put('fine-settings/create', 'store')->name('admin.fine-settings.store');
   });
 });
