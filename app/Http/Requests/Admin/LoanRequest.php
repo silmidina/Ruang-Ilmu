96a17a707419e9ce96eead3c881b0cca639132cd
silmidina <silmirhmdn08@gmail.com>
2025-02-27 18:03:38 +0700

    request validation

---
+++ app/Http/Requests/Admin/LoanRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user' => [
+                'required',
+                'exists:users,name',
+            ],
+            'book' => [
+                'required',
+                'exists:books,title',
+            ],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'user' => 'Pengguna',
+            'book' => 'Buku',
+        ];
+    }
+}
