47098f44524d95b65e8f0f05cef09ff8dd40a3ee
silmidina <silmirhmdn08@gmail.com>
2025-03-01 11:34:26 +0700

    scope filter dan sorting

--- app/Models/ReturnBook.php
+++ app/Models/ReturnBook.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Enums\ReturnBookStatus;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Builder;
 
 class ReturnBook extends Model
 {
@@ -44,5 +45,50 @@ class ReturnBook extends Model
     public function returnBookCheck(): HasOne
     {
         return $this->hasOne(ReturnBookCheck::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->whereAny([
+                    'return_book_code',
+                    'status',
+                ], 'REGEXP', $search);
+            })
+                ->orWhereHas('loan', fn($query) => $query->where('loan_code', 'REGEXP', $search))
+                ->orWhereHas('user', fn($query) => $query->where('name', 'REGEXP', $search))
+                ->orWhereHas('book', fn($query) => $query->where('title', 'REGEXP', $search));
+        });
+    }
+
+    public function scopeSorting(Builder $query, array $sorts): void
+    {
+        $query->when($sorts['field'] ?? null && $sorts['direction'] ?? null, function ($query) use ($sorts) {
+            match ($sorts['field']) {
+                'loan_code' => $query->whereHas('loan', fn($query) => $query->orderBy('loan_code', $sorts['direction'])),
+                default => $query->orderBy($sorts['field'], $sorts['direction']),
+            };
+        });
+    }
+
+    public function scopeReturned(Builder $query): Builder
+    {
+        return $query->where('status', ReturnBookStatus::RETURNED->value);
+    }
+
+    public function scopeFine(Builder $query): Builder
+    {
+        return $query->where('status', ReturnBookStatus::FINE->value);
+    }
+
+    public function scopeChecked(Builder $query): Builder
+    {
+        return $query->where('status', ReturnBookStatus::CHECKED->value);
+    }
+
+    public function scopeMember(Builder $query, int $user_id): Builder
+    {
+        return $query->where('user_id', $user_id);
     }
 }
