99633b42757c30b7cea2a291bbf8c216db74569d
silmidina <silmirhmdn08@gmail.com>
2025-03-02 23:42:09 +0700

    refactoring page update profile admin profile

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -2,6 +2,10 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
+import { Alert, AlertDescription } from '@/Components/ui/alert';
+import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card';
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/Components/ui/label';
 import { Transition } from '@headlessui/react';
 import { Link, useForm, usePage } from '@inertiajs/react';
 
@@ -13,79 +17,79 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
         email: user.email,
     });
 
-    const submit = (e) => {
+    const onHandleChange = (e) => setData(e.target.name, e.target.value)
+    const onHandleSubmit = (e) => {
         e.preventDefault();
 
         patch(route('profile.update'));
     };
 
     return (
-        <section className={className}>
-            <header>
-                <h2 className="text-lg font-medium text-gray-900">Profile Information</h2>
+        <Card className={className}>
+            <CardHeader>
+                <CardTitle className="text-lg font-medium text-gray-900">Profile Information</CardTitle>
 
-                <p className="mt-1 text-sm text-gray-600">
+                <CardDescription className="mt-1 text-sm text-gray-600">
                     Update your account's profile information and email address.
-                </p>
-            </header>
-
-            <form onSubmit={submit} className="mt-6 space-y-6">
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+            <form onSubmit={onHandleSubmit} className="mt-6 space-y-6">
                 <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
-                    <TextInput
+                    <Label htmlFor="name">Nama</Label>
+                    <Input
                         id="name"
-                        className="mt-1 block w-full"
+                        name="name"
                         value={data.name}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                        isFocused
+                        onChange={onHandleChange}
                         autoComplete="name"
                     />
-
-                    <InputError className="mt-2" message={errors.name} />
+                    {errors.name && (
+                        <InputError className="mt-2" message={errors.name} />
+                    )}
                 </div>
 
                 <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
+                    <Label htmlFor="email">Email</Label>
+                    <Input
                         id="email"
+                        name="email"
                         type="email"
-                        className="mt-1 block w-full"
                         value={data.email}
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
+                        onChange={onHandleSubmit}
                         autoComplete="username"
                     />
-
-                    <InputError className="mt-2" message={errors.email} />
+                    {errors.email && (
+                        <InputError className="mt-2" message={errors.email} />
+                    )}
                 </div>
 
                 {mustVerifyEmail && user.email_verified_at === null && (
                     <div>
-                        <p className="mt-2 text-sm text-gray-800">
+                        <p className="mt-2 text-sm text-foreground">
                             Your email address is unverified.
                             <Link
                                 href={route('verification.send')}
                                 method="post"
                                 as="button"
-                                className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                className="rounded-md text-sm text-muted-foreground underline hover:text-foreground focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
                             >
                                 Click here to re-send the verification email.
                             </Link>
                         </p>
 
                         {status === 'verification-link-sent' && (
-                            <div className="mt-2 text-sm font-medium text-green-600">
+                            <Alert variant="succes">
+                                <AlertDescription>
                                 A new verification link has been sent to your email address.
-                            </div>
+                                </AlertDescription>
+                            </Alert>
                         )}
                     </div>
                 )}
 
                 <div className="flex items-center gap-4">
-                    <PrimaryButton disabled={processing}>Save</PrimaryButton>
+                    <Button variant='orange' size='lg' disabled={processing}>Save</Button>
 
                     <Transition
                         show={recentlySuccessful}
@@ -94,10 +98,11 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
                         leave="transition ease-in-out"
                         leaveTo="opacity-0"
                     >
-                        <p className="text-sm text-gray-600">Saved.</p>
+                        <p className="text-sm text-muted-foreground">Saved.</p>
                     </Transition>
                 </div>
             </form>
-        </section>
+           </CardContent>
+        </Card>
     );
 }
