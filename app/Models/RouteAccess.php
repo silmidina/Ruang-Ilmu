0190363bc0e4541a789858176dd77615dbb0cc9c
silmidina <silmirhmdn08@gmail.com>
2025-03-05 02:29:58 +0700

    scope filter dan sorting

--- app/Models/RouteAccess.php
+++ app/Models/RouteAccess.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -23,5 +24,25 @@ class RouteAccess extends Model
     public function permission(): BelongsTo
     {
         return $this->belongsTo(Permission::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'], function ($query, $search) {
+            $query->where('route_name', 'REGEXP', $search)
+                ->orWhereHas('role', fn($query) => $query->where('name', 'REGEXP', $search))
+                ->orWhereHas('permission', fn($query) => $query->where('name', 'REGEXP', $search));
+        });
+    }
+
+    public function scopeSorting(Builder $query, array $sorts): void
+    {
+        $query->when($sorts['field'] ?? null && $sorts['direction'] ?? null, function ($query) use ($sorts) {
+            match ($sorts['field']) {
+                'role_id' => $query->whereHas('role', fn($query) => $query->orderBy('name', $sorts['direction'])),
+                'permission_id' => $query->whereHas('permission', fn($query) => $query->orderBy('name', $sorts['direction'])),
+                default => $query->orderBy($sorts['field'], $sorts['direction']),
+            };
+        });
     }
 }
