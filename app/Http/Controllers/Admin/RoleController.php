055e0369a5d843854d866f35d3cc43df06fae642
silmidina <silmirhmdn08@gmail.com>
2025-03-04 12:46:31 +0700

    tampilkan data role

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\RoleResource;
 use Illuminate\Http\Request;
+use Inertia\Response;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $roles = Role::query()
+            ->select(['id', 'name', 'guard_name', 'created_at'])
+            ->when(request()->search, function ($query, $search) {
+                $query->whereAny([
+                    'name',
+                    'guard_name',
+                ], 'REGEXP', $search);
+            })
+            ->when(request()->field && request()->direction, fn($query) => $query->orderBy(request()->field, request()->direction))
+            ->paginate(request()->load ?? 10)
+            ->withQueryString();
+
+        return inertia('Admin/Roles/Index', [
+            'page_settings' => [
+                'title' => 'Peran',
+                'subtitle' => 'Menampilkan semua data peran yang tersedia pada platform ini',
+            ],
+            'roles' => RoleResource::collection($roles)->additional([
+                'meta' => [
+                    'has_pages' => $roles->hasPages(),
+                ],
+            ]),
+            'state' => [
+                'page' => request()->page ?? 1,
+                'search' => request()->search ?? '',
+                'load' => 10,
+            ],
+        ]);
+    }
 }
--- resources/js/Layouts/Partials/Sidebar.jsx
+++ resources/js/Layouts/Partials/Sidebar.jsx
@@ -70,7 +70,7 @@ export default function Sidebar({ url, auth }) {
             />
 
             <div className="px-3 py-2 text-sm font-semibold text-foreground">Peran dan Izin</div>
-            <NavLink url="#" title="Peran" icon={IconCircleKey} />
+            <NavLink url={route('admin.roles.index')} active={url.startsWith('/admin/roles')} title="Peran" icon={IconCircleKey} />
             <NavLink url="#" title="Izin" icon={IconVersions} />
             <NavLink url="#" title="Tetapkan Izin" icon={IconKeyframe} />
             <NavLink url="#" title="Tetapkan Peran" icon={IconLayoutKanban} />
