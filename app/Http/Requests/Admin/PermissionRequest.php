9de144da11f9392d21683e44d42599a77430ab84
silmidina <silmirhmdn08@gmail.com>
2025-03-04 16:49:27 +0700

    request validation admin permissions

---
+++ app/Http/Requests/Admin/PermissionRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255',
+                Rule::unique('permissions')->ignore($this->permission),
+            ],
+            'guard_name' => [
+                'nullable',
+                'in:web,api',
+            ],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name' => 'Nama',
+            'guard_name' => 'Guard',
+        ];
+    }
+}
--- app/Http/Resources/Admin/PermissionResource.php
+++ app/Http/Resources/Admin/PermissionResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PermissionsResource extends JsonResource
+class PermissionResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
