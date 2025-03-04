fd08849d56ad607664ec781f59cd0910731cb703
silmidina <silmirhmdn08@gmail.com>
2025-03-05 00:58:37 +0700

    request validation admin assign users

---
+++ app/Http/Requests/Admin/AssignUserRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AssignUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'roles' => [
+                'nullable',
+                'exists:roles,id'
+            ],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'roles' => 'Peran',
+        ];
+    }
+}
