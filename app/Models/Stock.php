5637b01b451b32e178cd684589ad0fc9d7421dda
silmidina <silmirhmdn08@gmail.com>
2025-03-07 23:39:39 +0700

    scope filter dan sorting admin stok buku

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\Builder;
 
 class Stock extends Model
 {
@@ -19,5 +20,25 @@ class Stock extends Model
     public function book(): BelongsTo
     {
         return $this->belongsTo(Book::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->whereHas('book', fn($query) => $query->where('title', 'REGEXP', $search));
+            });
+        });
+    }
+
+    public function scopeSorting(Builder $query, array $sorts): void
+    {
+        $query->when($sorts['field'] ?? null && $sorts['direction'] ?? null, function ($query) use ($sorts) {
+            match ($sorts['field']) {
+                'book_id' => $query->join('books', 'stocks.book_id', '=', 'books.id')
+                    ->orderBy('books.title', $sorts['direction']),
+                default => $query->orderBy($sorts['field'], $sorts['direction']),
+            };
+        });
     }
 }
